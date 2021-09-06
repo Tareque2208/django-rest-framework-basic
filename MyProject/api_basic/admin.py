2eea49a067283bc63352fe28303618aa73313b51
Ahmed Tareque <tareque.bracu@gmail.com>
2021-09-06 13:42:18 +0600

    one to one and one to many relationship completed

--- MyProject/api_basic/admin.py
+++ MyProject/api_basic/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, ArticleRating, Course, Tag
 
 # Register your models here.
 
 admin.site.register(Article)
+admin.site.register(ArticleRating)
+admin.site.register(Tag)
+admin.site.register(Course)
--- MyProject/api_basic/models.py
+++ MyProject/api_basic/models.py
@@ -2,11 +2,38 @@ from django.db import models
 
 # Create your models here.
 
+class ArticleRating(models.Model):
+    likes=models.BigIntegerField(default=0)
+    dislikes=models.BigIntegerField(default=0)
+
+    def __int__(self):
+        return self.likes
+
+class Tag(models.Model):
+    title=models.CharField(max_length=100)
+    desc=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Course(models.Model):
+    title=models.CharField(max_length=100)
+    teacher=models.CharField(max_length=100)
+    tag=models.CharField(max_length=100)
+    date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.courseTitle
+
 class Article(models.Model):
     title=models.CharField(max_length=100)
     author=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     date=models.DateTimeField(auto_now_add=True)
-
+    #One to one
+    rating = models.OneToOneField(ArticleRating, on_delete=models.CASCADE, null=True)
+    #One to many
+    tags = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return self.title
+
